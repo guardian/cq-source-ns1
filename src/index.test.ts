7d78a6542a6cc9fe38a5d721cb45c60b1f51cae2
akash1810 <akash1810@users.noreply.github.com>
2023-09-15 11:13:47 +0100

    test: Remove Jest (temporarily)
    Jest + ESM isn't playing very nicely.
    There aren't any meaningful tests right now,
    so remove Jest for speed.
    
    ESM support in Jest is still experimental,
    so might need to use a different testing framework.

--- src/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { main } from './index';
-
-describe('The lambda', () => {
-	it('should return a greeting', async () => {
-		const response = await main();
-		expect(response).toContain('Hello! The time is');
-	});
-});
