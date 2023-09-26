5a988af43eb8fc33b800388fe8281ddfe25b3c2a
akash1810 <akash1810@users.noreply.github.com>
2023-09-26 14:24:21 +0100

    feat: Add a simple table, with dummy data

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 A [CloudQuery](https://www.cloudquery.io/) source plugin for [NS1](https://ns1.com/).
 
 Written in TypeScript using the recently added [CloudQuery JavaScript SDK](https://www.cloudquery.io/blog/announcing-cloudquery-javascript-sdk).
+
+## Developing
+1. Start the plugin via `npm start`
+2. Use the plugin via `npm run test:integration`
--- src/tables.ts
+++ src/tables.ts
@@ -1,4 +1,12 @@
-import type { Table } from '@cloudquery/plugin-sdk-javascript/schema/table';
+import type { Writable } from 'stream';
+import { Utf8 } from '@cloudquery/plugin-sdk-javascript/arrow';
+import { createColumn } from '@cloudquery/plugin-sdk-javascript/schema/column';
+import { pathResolver } from '@cloudquery/plugin-sdk-javascript/schema/resolvers';
+import { createTable } from '@cloudquery/plugin-sdk-javascript/schema/table';
+import type {
+	Table,
+	TableResolver,
+} from '@cloudquery/plugin-sdk-javascript/schema/table';
 import type { Logger } from 'winston';
 import type { Spec } from './spec.js';
 
@@ -6,6 +14,37 @@ export const getTables = async (
 	logger: Logger,
 	spec: Spec,
 ): Promise<Table[]> => {
-	// TOD add tables
-	return Promise.resolve([]);
+	const resolver: TableResolver = async (
+		clientMeta: unknown,
+		parent: unknown,
+		stream: Writable,
+	) => {
+		await new Promise((_) => setTimeout(_, 500));
+
+		const data = new Array(10).fill(undefined);
+		data.forEach((_, index) => {
+			stream.write({ id: [index], name: [`hello ${index}`] });
+		});
+		return;
+	};
+
+	const table = createTable({
+		name: 'test',
+		columns: [
+			createColumn({
+				name: 'id',
+				type: new Utf8(),
+				resolver: pathResolver('id'),
+			}),
+			createColumn({
+				name: 'name',
+				type: new Utf8(),
+				resolver: pathResolver('name'),
+			}),
+		],
+		description: 'testing',
+		resolver,
+	});
+
+	return Promise.resolve([table]);
 };
